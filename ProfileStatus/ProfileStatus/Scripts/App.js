b0e94d8781d9e5242ede600712b4796aeff256bb
oscararnez <oscar.arnez@altran.com>
2014-06-02 00:17:22 +0200

    Added functionality to get the email of the current user

--- ProfileStatus/ProfileStatus/Scripts/App.js
+++ ProfileStatus/ProfileStatus/Scripts/App.js
@@ -3,6 +3,7 @@ var ACSAppPart = window.ACSAppPart || {}
 
 var context = SP.ClientContext.get_current();
 var user = context.get_web().get_currentUser();
+var um;
 
 // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
 $(document).ready(function () {
@@ -11,7 +12,9 @@ $(document).ready(function () {
 
 // This function prepares, loads, and then executes a SharePoint query to get the current users information
 function getUserName() {
+   
     context.load(user);
+
     context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
 }
 
@@ -19,11 +22,22 @@ function getUserName() {
 // It replaces the contents of the 'message' element with the user name
 function onGetUserNameSuccess() {
     //$('#message').text('Hello ' + user.get_title());
-    var usernamex = user.get_title();
-    ACSAppPart.ProfileStatusViewModel.load(usernamex);
+    
+    um = user.get_email();
+    ACSAppPart.ProfileStatusViewModel.load(um);
 }
 
 // This function is executed if the above call fails
 function onGetUserNameFail(sender, args) {
     alert('Failed to get user name. Error:' + args.get_message());
+}
+
+function getQueryStringParameter(urlParameterKey) {
+    var params = document.URL.split('?')[1].split('&');
+    var strParams = '';
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split('=');
+        if (singleParam[0] == urlParameterKey)
+            return decodeURIComponent(singleParam[1]);
+    }
 }

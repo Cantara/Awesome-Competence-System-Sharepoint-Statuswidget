36d9c247c93ef67f9b9a438e3cd4f14667dc1c4c
oscararnez <oscar.arnez@altran.com>
2014-06-02 00:14:58 +0200

    ProfileStatus updated

--- ProfileStatus/ProfileStatus/Scripts/ProfileStatus.js
+++ ProfileStatus/ProfileStatus/Scripts/ProfileStatus.js
@@ -1,26 +1,25 @@
 ﻿"use strict"
 var ACSAppPart = window.ACSAppPart || {}
-window.ACSAppPart.ProfileStatus = function (pc, lu) {
+window.ACSAppPart.ProfileStatus = function (fl,pc, lu) {
     // Private members
-    var cstatus = 'undefined'
-    var percent = 'undefined'
-    var lastupdated = 'undefined'
+    var percent = 'undefined',
+        lastupdated = 'undefined',
+        statusflag = 'undefined',
+        get_percent = function () { return percent; },
+        get_lastupdated = function () { return lastupdated; },
+        get_statusflag = function () { return statusflag; };
 
     // Constructor
+    // Status flag
+    statusflag = fl;
     // Profile completeness in percent
     percent = pc;
     // Date when the profile was last updated
     lastupdated = lu;
-    // Number of days since last update
-    cstatus = Math.ceil((new Date().getTime() - lu.getTime()) / (1000 * 60 * 60 * 24));
-
-    get_percent = function () { return percent; },
-    get_lastupdated = function () { return lastupdated; },
-    get_cstatus = function () { return cstatus; };
 
     // Public interface
     return {
-        get_cstatus: get_cstatus,
+        get_statusflag: get_statusflag,
         get_percent: get_percent,
         get_lastupdated: get_lastupdated
     };
